9574b4d383d0afb18c60fa48d54f2ec92236aac6
adityaparikh <aditya.parikh@iet.ahduni.edu.in>
2015-01-25 22:08:32 +0530

    lab2

---
+++ sub2.sh
@@ -0,0 +1,27 @@
+uname -n > output.txt
+factor 65 >> output.txt
+diff Sample1.txt Sample2.txt >> output.txt
+du -H >> output.txt
+tty >> output.txt
+who -H >> output.txt
+df >> output.txt
+iw >> output.txt
+rev Sample1.txt >> output.txt
+seq 2 20 >> output.txt
+unexpand Sample1.txt >> output.txt
+sync >> output.txt
+ifconfig >> output.txt
+ps -u >> output.txt
+who -b >> output.txt
+uname -a >> output.txt
+free >> output.txt
+su root
+init >> output.txt
+dmesg >> output.txt
+free -m >> output.txt
+bzip2 Sample1.txt >> output.txt
+
+
+
+
+
